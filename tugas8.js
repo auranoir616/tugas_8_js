d0cf64b3c5e0bb929d139da6c8bba2ec088ce157
febri <fitrafilthmongers@gmail.com>
2023-07-02 22:19:22 +0700

    tugas 8 js
    mengumpulkan tugas 8 js

---
+++ tugas8.js
@@ -0,0 +1,10 @@
+var kucing ={
+    jenis: 'kucing kampung',
+    warna: 'hitam belang',
+    kelamin: 'jantan',
+    makanan: 'ikan',
+    umur: '5 tahun',
+}
+kucing.warna= 'orange'
+
+console.log(kucing)
